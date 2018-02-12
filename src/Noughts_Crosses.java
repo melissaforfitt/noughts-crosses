cd71d80981741d143b2ce098222a7dcdba402905
melissa.forfitt <melissa.forfitt@10.4.193.20>
2018-02-12 12:57:59 +0000

    Created 3x3 grid of buttons.

--- src/Noughts_Crosses.java
+++ src/Noughts_Crosses.java
@@ -3,30 +3,56 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class Noughts_Crosses extends Application {
+
+	// Build grid
+	GridPane grid = new GridPane();
+
+	// Make 9 buttons
+	public void buildGrid() {
+		final Button[][] b = new Button[3][3];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				b[i][j] = new Button();
+				b[i][j].setMinWidth(50);
+				b[i][j].setMinHeight(50);
+				grid.add(b[i][j], i, j);
+			}
+		}
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
 
-		GridPane grid = new GridPane();
-		Button foo = new Button("");
-		grid.add(foo, 0, 0);
+		buildGrid();
 
-		Scene scene = new Scene(grid, 250, 250);
+		// Change button text when clicked
+		b.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				b.setText("X");
+				b.setText("O");
+			}
+		});
 		
+
+		// Create scene
+		Scene scene = new Scene(grid, 150, 150);
+
 		primaryStage.setTitle("Noughts and Crosses");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
+
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
-		
+
+		// Show graphics
 		launch(args);
-		
+
 	}
 
 }
