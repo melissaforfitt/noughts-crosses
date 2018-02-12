d58f83cd78e1bb35e0cb5637250438e522b7095e
Melissa <Melissa@192.168.169.70>
2018-02-12 16:22:10 +0000

    Added more methods.

--- src/Noughts_Crosses.java
+++ src/Noughts_Crosses.java
@@ -1,5 +1,6 @@
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.application.Application;
@@ -8,21 +9,42 @@ import javafx.event.EventHandler;
 
 public class Noughts_Crosses extends Application {
 
+	// Build grid
+	GridPane grid = new GridPane();
+
 	public void buildGrid() {
-		
-		// Build grid
-		GridPane grid = new GridPane();
-		
-		// Make 9 buttons
-		final Button[][] b = new Button[3][3];
+
+	}
+
+	public boolean markedX() {
+
+		boolean x = true;
+
+		return x;
+
+	}
+
+	public boolean markedY() {
+
+		boolean y = true;
+
+		return y;
+
+	}
+
+	public boolean end() {
+
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				b[i][j] = new Button();
-				b[i][j].setMinWidth(100);
-				b[i][j].setMinHeight(100);
-				grid.add(b[i][j], i, j);
-			}
+				if ()
+				boolean end = true;
+			} boolean end = false;
 		}
+
+		
+
+		return end;
+
 	}
 
 	@Override
@@ -30,24 +52,35 @@ public class Noughts_Crosses extends Application {
 
 		buildGrid();
 		
-		Button clicked = new Button();
+		// Make 9 buttons
+		Button[][] b = new Button[3][3];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				b[i][j] = new Button();
+				b[i][j].setMinWidth(100);
+				b[i][j].setMinHeight(100);
+				grid.add(b[i][j], i, j);
 
+			}
+		}
+		
 		// Change button text when clicked
-		b.setOnAction(new EventHandler<ActionEvent>() {
+		b[i][j].setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				b.setText("X");
-				b.setText("O");
+				b[i][j].setText("X");
+				markedX();
+				b[i][j].setText("O");
+				markedY();
 			}
-		});
-		
+		}
 
-		// Create scene
-		Scene scene = new Scene(grid, 300, 300);
+	// Create scene
+	Scene scene = new Scene(grid, 300, 300);
 
-		primaryStage.setTitle("Noughts and Crosses");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+	primaryStage.setTitle("Noughts and Crosses");
+	primaryStage.setScene(scene);
+	primaryStage.show();
 
 	}
 
