ff1f4a7a3700f17fcf302a64471ce437463b9c7b
melissa.forfitt <melissa.forfitt@10.4.193.20>
2018-02-12 13:07:41 +0000

    Moved grid to separate method.

--- src/Noughts_Crosses.java
+++ src/Noughts_Crosses.java
@@ -8,17 +8,18 @@ import javafx.event.EventHandler;
 
 public class Noughts_Crosses extends Application {
 
-	// Build grid
-	GridPane grid = new GridPane();
-
-	// Make 9 buttons
 	public void buildGrid() {
+		
+		// Build grid
+		GridPane grid = new GridPane();
+		
+		// Make 9 buttons
 		final Button[][] b = new Button[3][3];
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				b[i][j] = new Button();
-				b[i][j].setMinWidth(50);
-				b[i][j].setMinHeight(50);
+				b[i][j].setMinWidth(100);
+				b[i][j].setMinHeight(100);
 				grid.add(b[i][j], i, j);
 			}
 		}
@@ -28,6 +29,8 @@ public class Noughts_Crosses extends Application {
 	public void start(Stage primaryStage) {
 
 		buildGrid();
+		
+		Button clicked = new Button();
 
 		// Change button text when clicked
 		b.setOnAction(new EventHandler<ActionEvent>() {
@@ -40,7 +43,7 @@ public class Noughts_Crosses extends Application {
 		
 
 		// Create scene
-		Scene scene = new Scene(grid, 150, 150);
+		Scene scene = new Scene(grid, 300, 300);
 
 		primaryStage.setTitle("Noughts and Crosses");
 		primaryStage.setScene(scene);
