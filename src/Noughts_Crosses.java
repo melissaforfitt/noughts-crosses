bf5d614273ef7e59998e9f81ddc2ccdbe973f7a2
melissa.forfitt <melissa.forfitt@10.4.193.20>
2018-02-12 12:18:25 +0000

    Set up gridpane.

--- src/Noughts_Crosses.java
+++ src/Noughts_Crosses.java
@@ -1,11 +1,32 @@
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.application.Application;
 
-public class Noughts_Crosses {
+public class Noughts_Crosses extends Application {
 
+	@Override
+	public void start(Stage primaryStage) {
+
+		GridPane grid = new GridPane();
+		Button foo = new Button("");
+		grid.add(foo, 0, 0);
+
+		Scene scene = new Scene(grid, 250, 250);
+		
+		primaryStage.setTitle("Noughts and Crosses");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
+		launch(args);
 		
-		
-
 	}
 
 }
