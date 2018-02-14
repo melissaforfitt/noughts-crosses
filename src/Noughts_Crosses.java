a46f3ad170c15c42a4351dfe699efae28c5e4882
Melissa <Melissa@192.168.170.3>
2018-02-14 13:46:40 +0000

    Made buttons clickable.

--- src/Noughts_Crosses.java
+++ src/Noughts_Crosses.java
@@ -1,19 +1,22 @@
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class Noughts_Crosses extends Application {
 
+	Button b[][] = new Button[3][3];
+
 	public void buildGrid() {
 
 	}
 
-	public boolean markedX() { 
+	public boolean markedX() {
 
 		boolean x = true;
 
@@ -33,12 +36,11 @@ public class Noughts_Crosses extends Application {
 
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				if ()
-				boolean end = true;
-			} 
-		} boolean end = false;
-
-		
+				// if ()
+				// boolean end = true;
+			}
+		}
+		boolean end = false;
 
 		return end;
 
@@ -48,12 +50,12 @@ public class Noughts_Crosses extends Application {
 	public void start(Stage primaryStage) {
 
 		buildGrid();
-		
+		int turnCounter = 0;
+
 		// Build grid
 		GridPane grid = new GridPane();
-		
+
 		// Make 9 buttons
-		Button[][] b = new Button[3][3];
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				b[i][j] = new Button();
@@ -61,27 +63,28 @@ public class Noughts_Crosses extends Application {
 				b[i][j].setMinHeight(100);
 				grid.add(b[i][j], i, j);
 
+				int x = i;
+				int y = j;
+				
+				b[i][j].setOnAction(new EventHandler<ActionEvent>() {
+					public void handle(ActionEvent e) {
+						if (turnCounter % 2 == 0) {
+							b[x][y].setText("X");
+						} else
+							b[x][y].setText("Y");
+						System.out.println(turnCounter);
+					}
+				});
+
 			}
 		}
-		
-		// Change button text when clicked
-		b[i][j].setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				b[i][j].setText("X");
-				markedX();
-				b[i][j].setText("O");
-				markedY();
-			}
-		}
 
-	// Create scene
-	Scene scene = new Scene(grid, 300, 300);
+		// Create scene
+		Scene scene = new Scene(grid, 300, 300);
 
-	primaryStage.setTitle("Noughts and Crosses");
-	primaryStage.setScene(scene);
-	primaryStage.show();
-
+		primaryStage.setTitle("Noughts and Crosses");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
